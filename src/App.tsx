3bb46b0d0fb701b7a48a111be7aa831cd188d7ca
Ihor Nikora <ihor.nikora@gmail.com>
2021-09-26 23:55:47 +0300

    enable reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import Game from "./components/Game/Game";
-
 import "./App.scss";
 
 function App() {
+  const [ms, setMs] = useState<number>(Date.now());
+  const resetHandler = () => setMs(Date.now());
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Play 2048 Game</h1>
-        <button className="button" type="reset">Reset</button>
+        <button className="button" type="reset" onClick={resetHandler}>
+          Reset
+        </button>
       </header>
-      <Game />
+      <Game key={ms} />
     </div>
   );
 }
