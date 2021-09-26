7c95908154c781da8f48364dbf9ae57a63704061
Ihor Nikora <ihor.nikora@gmail.com>
2021-09-26 23:14:18 +0300

    add arrow key press handlers

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,7 +1,37 @@
+import { useEffect } from "react";
+import { useThrottledCallback } from "use-debounce";
 import Board from "../Board/Board";
 // import styles from "./Game.module.scss";
 
 const Game = () => {
+  const handleKeyDown = (e: KeyboardEvent) => {
+    e.preventDefault();
+
+    switch (e.code) {
+      case "ArrowLeft":
+        return console.log("ArrowLeft");
+      case "ArrowRight":
+        return console.log("ArrowRight");
+      case "ArrowUp":
+        return console.log("ArrowUp");
+      case "ArrowDown":
+        return console.log("ArrowDown");
+    }
+  };
+
+  const throttledHandleKeyDown = useThrottledCallback(handleKeyDown, 250, {
+    leading: true,
+    trailing: false,
+  });
+
+  useEffect(() => {
+    window.addEventListener("keydown", throttledHandleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", throttledHandleKeyDown);
+    };
+  }, [throttledHandleKeyDown]);
+
   return <Board size={4} />;
 };
 
